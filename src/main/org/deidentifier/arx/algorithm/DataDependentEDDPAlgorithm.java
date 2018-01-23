9b8a3f73a6bbb444e19b09377360d2cfbc8f9127
RaffaelBild <raffael_bild@gmx.de>
2018-01-23 22:05:14 +0100

    Measure information loss of the result according to the metric

--- src/main/org/deidentifier/arx/algorithm/DataDependentEDDPAlgorithm.java
+++ src/main/org/deidentifier/arx/algorithm/DataDependentEDDPAlgorithm.java
@@ -163,6 +163,7 @@ public class DataDependentEDDPAlgorithm extends AbstractAlgorithm{
         
         // Track optimum
         trackOptimum(bestTransformation);
+        bestTransformation.setChecked(checker.check(bestTransformation, true, ScoreType.CONVENTIONAL));
         return false;
     }
 
